fc9d7851b91766975550834e74b80b19a5968f40
JCR21598 <juan.c.rodriguez.viteri@gmail.com>
2026-03-03 15:55:08 +0000

    Making run_tests.sh functional again.

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
-for input in ../../../inputs/*.in; do
+echo "Compiling..."
+
+g++-15 -fopenmp g3/serial/src/docs.cpp -o docs
+
+echo "Running tests..."
+
+for input in inputs/*.in; do
   base="${input%.in}"
   echo "==> ${base##*/}"
+
   ./docs "$input" > "$base-new.out"
   diff -u "$base.out" "$base-new.out"
 done

96d26a462f6c16ef47310badf4a2fba618e1acf5
JCR21598 <juan.c.rodriguez.viteri@gmail.com>
2026-03-03 16:45:02 +0000

    Removing compilation from run_tests.sh to incentivise to building executable.

--- run_tests.sh
+++ run_tests.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Compiling..."
-
-g++-15 -fopenmp g3/serial/src/docs.cpp -o docs
-
 echo "Running tests..."
 
 for input in inputs/*.in; do
   base="${input%.in}"
   echo "==> ${base##*/}"
 
-  ./docs "$input" > "$base-new.out"
+  ./g3/serial/src/docs "$input" > "$base-new.out"
   diff -u "$base.out" "$base-new.out"
 done
 

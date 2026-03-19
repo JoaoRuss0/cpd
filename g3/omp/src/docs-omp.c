b2b74b666ce2586f006db07a31c0fada252fcf37
João Russo <r0u6s12s18o@gmail.com>
2026-03-19 22:51:20 +0000

    feat: reformat code

--- g3/omp/src/docs-omp.c
+++ g3/omp/src/docs-omp.c
@@ -1,11 +1,11 @@
+#include <math.h>
+#include <omp.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <math.h>
-#include <stdbool.h>
-#include <omp.h>
+#include <string.h>
 
 #define CACHE_LINE_SIZE_IN_BYTES 64
 #define SIZE_T_PER_CACHE_LINE (CACHE_LINE_SIZE_IN_BYTES / sizeof(size_t))
@@ -33,15 +33,19 @@ struct Problem {
     double *document_scores;
 };
 
-size_t get_closest_cabinet_index(const Cabinets* cabinets, size_t parent_id,  double* document_scores, size_t subject_count);
+size_t get_closest_cabinet_index(const Cabinets *cabinets, size_t parent_id,
+                                 double *document_scores, size_t subject_count);
 
-void recalculate_scores(const Cabinets* cabinets, size_t subject_count);
+void recalculate_scores(const Cabinets *cabinets, size_t subject_count);
 
-void assign_to_cabinets(const Cabinets *cabinets, const Documents *documents, size_t subject_count);
+void assign_to_cabinets(const Cabinets *cabinets, const Documents *documents,
+                        size_t subject_count);
 
-double calculate_distance(size_t subject_count, const double *score_1, const double *score_2);
+double calculate_distance(size_t subject_count, const double *score_1,
+                          const double *score_2);
 
-void reassign_documents(const Cabinets *cabinets, const Documents *documents, size_t subject_count);
+void reassign_documents(const Cabinets *cabinets, const Documents *documents,
+                        size_t subject_count);
 
 void free_problem(const Problem *p);
 
@@ -55,9 +59,9 @@ void print_result(const Documents *documents);
 
 int init_documents(const Problem *problem, Documents *documents);
 
-int init_cabinets(Problem* problem, Cabinets* cabinets);
+int init_cabinets(Problem *problem, Cabinets *cabinets);
 
-int init_auxiliary_arrays(Problem* problem);
+int init_auxiliary_arrays(Problem *problem);
 
 int swaps = 0;
 size_t *new_counts = NULL;
@@ -90,15 +94,15 @@ int main(const int argc, char *argv[]) {
     exec_time = -omp_get_wtime();
 
 #pragma omp parallel
-{
-    thread_number = omp_get_thread_num();
+    {
+        thread_number = omp_get_thread_num();
 
-    assign_to_cabinets(&cabinets, &documents, problem.subject_count);
-    do {
-        recalculate_scores(&cabinets, problem.subject_count);
-        reassign_documents(&cabinets, &documents, problem.subject_count);
-    } while (swaps > 0);
-}
+        assign_to_cabinets(&cabinets, &documents, problem.subject_count);
+        do {
+            recalculate_scores(&cabinets, problem.subject_count);
+            reassign_documents(&cabinets, &documents, problem.subject_count);
+        } while (swaps > 0);
+    }
 
     exec_time += omp_get_wtime();
 
@@ -115,8 +119,8 @@ cleanup:
     return 0;
 }
 
-void assign_to_cabinets(const Cabinets *cabinets, const Documents *documents, const size_t subject_count) {
-
+void assign_to_cabinets(const Cabinets *cabinets, const Documents *documents,
+                        const size_t subject_count) {
 #pragma omp for schedule(static)
     for (size_t i = 0; i < documents->count; i++) {
         const size_t index = i % cabinets->count;
@@ -124,21 +128,23 @@ void assign_to_cabinets(const Cabinets *cabinets, const Documents *documents, co
 
         new_counts[thread_number * padded_count_per_thread + index] += 1;
         for (size_t j = 0; j < subject_count; j++) {
-            new_scores[thread_number * padded_cabinet_subject_scores_per_thread + index * subject_count + j] += documents->scores[i * subject_count + j];
+            new_scores[thread_number *
+                           padded_cabinet_subject_scores_per_thread +
+                       index * subject_count + j] +=
+                documents->scores[i * subject_count + j];
         }
     }
 }
 
-void recalculate_scores(const Cabinets* cabinets, const size_t subject_count) {
-
-    #pragma omp for schedule(static)
+void recalculate_scores(const Cabinets *cabinets, const size_t subject_count) {
+#pragma omp for schedule(static)
     for (size_t c = 0; c < cabinets->count; c++) {
         size_t count = 0;
         for (size_t t = 0; t < n_threads; t++) {
             count += new_counts[t * padded_count_per_thread + c];
         }
 
-        double* score = &cabinets->scores[c * padded_subject_scores];
+        double *score = &cabinets->scores[c * padded_subject_scores];
         if (count == 0) {
             memset(score, 0, subject_count * sizeof(double));
             continue;
@@ -147,45 +153,58 @@ void recalculate_scores(const Cabinets* cabinets, const size_t subject_count) {
         for (size_t s = 0; s < subject_count; s++) {
             double sum = 0.0;
             for (size_t t = 0; t < n_threads; t++) {
-                sum += new_scores[t * padded_cabinet_subject_scores_per_thread + c * subject_count + s];
+                sum += new_scores[t * padded_cabinet_subject_scores_per_thread +
+                                  c * subject_count + s];
             }
-             score[s] = sum / (double)count;
+            score[s] = sum / (double)count;
         }
     }
 }
 
-void reassign_documents(const Cabinets *cabinets, const Documents *documents, const size_t subject_count) {
-
+void reassign_documents(const Cabinets *cabinets, const Documents *documents,
+                        const size_t subject_count) {
 #pragma omp single
     swaps = 0;
 
-    memset(&new_scores[thread_number * padded_cabinet_subject_scores_per_thread], 0, padded_cabinet_subject_scores_per_thread * sizeof(double));
-    memset(&new_counts[thread_number * padded_count_per_thread], 0, padded_count_per_thread * sizeof(size_t));
+    memset(
+        &new_scores[thread_number * padded_cabinet_subject_scores_per_thread],
+        0, padded_cabinet_subject_scores_per_thread * sizeof(double));
+    memset(&new_counts[thread_number * padded_count_per_thread], 0,
+           padded_count_per_thread * sizeof(size_t));
 
-#pragma omp for schedule(static) reduction(+:swaps)
+#pragma omp for schedule(static) reduction(+ : swaps)
     for (size_t i = 0; i < documents->count; i++) {
         const size_t old_cabinet_index = documents->parent_ids[i];
-        const size_t new_cabinet_index = get_closest_cabinet_index(cabinets, documents->parent_ids[i], &documents->scores[i * subject_count], subject_count);
+        const size_t new_cabinet_index = get_closest_cabinet_index(
+            cabinets, documents->parent_ids[i],
+            &documents->scores[i * subject_count], subject_count);
 
         if (new_cabinet_index != old_cabinet_index) {
             swaps += 1;
         }
 
         documents->parent_ids[i] = new_cabinet_index;
-        new_counts[thread_number * padded_count_per_thread + new_cabinet_index] += 1;
+        new_counts[thread_number * padded_count_per_thread +
+                   new_cabinet_index] += 1;
 
         for (size_t j = 0; j < subject_count; j++) {
-            new_scores[thread_number * padded_cabinet_subject_scores_per_thread + new_cabinet_index * subject_count + j] += documents->scores[i * subject_count + j];
+            new_scores[thread_number *
+                           padded_cabinet_subject_scores_per_thread +
+                       new_cabinet_index * subject_count + j] +=
+                documents->scores[i * subject_count + j];
         }
     }
 }
 
-size_t get_closest_cabinet_index(const Cabinets* cabinets, size_t parent_id,  double* document_scores, size_t subject_count) {
-
+size_t get_closest_cabinet_index(const Cabinets *cabinets, size_t parent_id,
+                                 double *document_scores,
+                                 size_t subject_count) {
     double min_distance = INFINITY;
 
     for (size_t j = 0; j < cabinets->count; j++) {
-        const double distance = calculate_distance(subject_count,&cabinets->scores[j * padded_subject_scores],document_scores);
+        const double distance = calculate_distance(
+            subject_count, &cabinets->scores[j * padded_subject_scores],
+            document_scores);
 
         if (distance < min_distance) {
             min_distance = distance;
@@ -196,7 +215,8 @@ size_t get_closest_cabinet_index(const Cabinets* cabinets, size_t parent_id,  do
     return parent_id;
 }
 
-double calculate_distance(const size_t subject_count, const double *score_1, const double *score_2) {
+double calculate_distance(const size_t subject_count, const double *score_1,
+                          const double *score_2) {
     double sum = 0;
 
     for (size_t i = 0; i < subject_count; i++) {
@@ -223,12 +243,14 @@ bool parse_problem(const char *filename, Problem *p) {
 
     char line[1024];
     if (!fgets(line, 1024, file) ||
-        !sscanf(line, "%zu %zu %zu", &p->cabinet_count, &p->document_count, &p->subject_count)) {
+        !sscanf(line, "%zu %zu %zu", &p->cabinet_count, &p->document_count,
+                &p->subject_count)) {
         status = false;
         goto cleanup;
     }
 
-    p->document_scores = (double *) calloc(p->document_count * p->subject_count, sizeof(double));
+    p->document_scores =
+        (double *)calloc(p->document_count * p->subject_count, sizeof(double));
     if (!p->document_scores) goto cleanup;
 
     for (size_t i = 0; i < p->document_count; i++) {
@@ -254,7 +276,7 @@ bool parse_problem(const char *filename, Problem *p) {
     }
 
 cleanup:
-    if (file)fclose(file);
+    if (file) fclose(file);
     if (!status) free_problem(p);
 
     return status;
@@ -279,7 +301,6 @@ inline size_t get_allocation_size(size_t single_count, size_t block_size) {
 }
 
 int init_documents(const Problem *problem, Documents *documents) {
-
     documents->count = problem->document_count;
     documents->scores = problem->document_scores;
 
@@ -288,29 +309,27 @@ int init_documents(const Problem *problem, Documents *documents) {
     return 0;
 }
 
-int init_cabinets(Problem* problem, Cabinets* cabinets) {
-
+int init_cabinets(Problem *problem, Cabinets *cabinets) {
     cabinets->count = problem->cabinet_count;
 
     padded_subject_scores =
-            get_allocation_size(problem->subject_count, DOUBLE_PER_CACHE_LINE);
-    cabinets->scores = (double*) get_cache_aligned_pointer(
+        get_allocation_size(problem->subject_count, DOUBLE_PER_CACHE_LINE);
+    cabinets->scores = (double *)get_cache_aligned_pointer(
         cabinets->count * padded_subject_scores * sizeof(double));
     if (!cabinets->scores) return 1;
     return 0;
 }
 
-int init_auxiliary_arrays(Problem* problem) {
-
+int init_auxiliary_arrays(Problem *problem) {
     padded_count_per_thread =
         get_allocation_size(problem->cabinet_count, SIZE_T_PER_CACHE_LINE);
-    new_counts = (size_t*) get_cache_aligned_pointer(n_threads * padded_count_per_thread *
-                                           sizeof(size_t));
+    new_counts = (size_t *)get_cache_aligned_pointer(
+        n_threads * padded_count_per_thread * sizeof(size_t));
     if (!new_counts) return 1;
 
     padded_cabinet_subject_scores_per_thread = get_allocation_size(
         problem->cabinet_count * problem->subject_count, DOUBLE_PER_CACHE_LINE);
-    new_scores = (double*) get_cache_aligned_pointer(
+    new_scores = (double *)get_cache_aligned_pointer(
         n_threads * padded_cabinet_subject_scores_per_thread * sizeof(double));
     if (!new_scores) return 1;
     return 0;

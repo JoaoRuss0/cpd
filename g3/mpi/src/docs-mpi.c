242000d5e90e67498efc9c9ea3c47e5e43a1229d
João Russo <r0u6s12s18o@gmail.com>
2026-03-29 23:04:39 +0100

    feat: add checkerboard version

--- g3/mpi/src/docs-mpi.c
+++ g3/mpi/src/docs-mpi.c
@@ -1,15 +1,16 @@
 #include <math.h>
+#include <mpi.h>
+#include <omp.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <mpi.h>
-#include <omp.h>
 
 typedef struct Cabinets Cabinets;
 typedef struct Documents Documents;
 typedef struct Problem Problem;
+typedef struct Change Change;
 
 struct Documents {
     size_t count;
@@ -21,6 +22,7 @@ struct Cabinets {
     size_t count;
     size_t *doc_count;
     double *scores;
+    size_t *doc_ids;
 };
 
 struct Problem {
@@ -30,10 +32,15 @@ struct Problem {
     double *document_scores;
 };
 
-size_t get_closest_cabinet_index(size_t parent_id, double *document_scores);
+struct Change {
+    size_t cab_idx;
+    double distance;
+};
+
+size_t get_closest_local_cabinet_index(size_t parent_id, double *document_scores, double* distance);
 void assign_to_cabinets();
 double calculate_distance(double *score_1, double *score_2);
-int reassign_documents();
+void compute_best_local_cabinet_for_documents();
 void increment_cabinet_scores(size_t cab_idx, size_t doc_idx);
 void decrement_cabinet_scores(size_t cab_idx, size_t doc_idx);
 void update_cabinets(size_t cab_idx, size_t doc_idx, int to_add);
@@ -42,20 +49,44 @@ bool parse_problem(char *filename, Problem *p);
 bool init_cabinets(Problem *problem);
 bool init_documents(Problem *problem);
 void print_result();
-void split_documents_by_rank();
+void compute_comms();
 void gather_parent_ids();
+bool init_changes();
+void compute_partition(size_t total, int num_parts, int part_id, size_t *start, size_t *end, size_t *count);
+int assign_best_cabinet();
+void recompute_scores();
 
 Documents documents;
 Cabinets cabinets;
 size_t subject_count;
 
 int rank;
-int nprocs;
+int n_procs;
+
+int row;
+int col;
+int grid_rows;
+int grid_cols;
+MPI_Comm row_comm, col_comm;
+
 size_t doc_s;
 size_t doc_e;
+size_t local_doc_count;
 
-size_t* local_count;
-double_t* local_sum;
+size_t cab_s;
+size_t cab_e;
+size_t local_cab_count;
+
+Change* local_changes;
+Change* global_changes;
+
+size_t *local_count;
+double *local_sum;
+int local_swaps;
+
+size_t *global_count;
+double *global_sum;
+int global_swaps;
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -63,11 +94,10 @@ int main(int argc, char *argv[]) {
     }
 
     MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+    MPI_Comm_size(MPI_COMM_WORLD, &n_procs);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     double exec_time;
-    int swaps = 0;
 
     Problem problem;
     problem.document_scores = NULL;
@@ -77,28 +107,34 @@ int main(int argc, char *argv[]) {
         goto cleanup;
 
     subject_count = problem.subject_count;
-    split_documents_by_rank();
+    compute_comms();
 
     local_count = calloc(cabinets.count, sizeof(size_t));
     local_sum = calloc(cabinets.count * subject_count, sizeof(double));
 
-    size_t* global_count = calloc(cabinets.count, sizeof(size_t));
-    double* global_sum = calloc(cabinets.count * subject_count, sizeof(double));
-    int global_swaps = 0;
+    global_count = calloc(cabinets.count, sizeof(size_t));
+    global_sum = calloc(cabinets.count * subject_count, sizeof(double));
+    global_swaps = 0;
+
+    compute_partition(documents.count, grid_rows, row, &doc_s, &doc_e, &local_doc_count);
+    compute_partition(cabinets.count, grid_cols, col, &cab_s, &cab_e, &local_cab_count);
+
+    if (!init_changes()) goto cleanup;
 
     exec_time = -omp_get_wtime();
 
     assign_to_cabinets();
     do {
-        swaps = reassign_documents();
+        compute_best_local_cabinet_for_documents();
+        MPI_Allgather(local_changes, (int)(local_doc_count * sizeof(Change)), MPI_BYTE,
+              global_changes, (int)(local_doc_count * sizeof(Change)), MPI_BYTE,
+              row_comm);
+        local_swaps = assign_best_cabinet();
 
-        MPI_Allreduce(local_sum, global_sum, cabinets.count * subject_count, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-        MPI_Allreduce(local_count, global_count, cabinets.count, MPI_UNSIGNED_LONG, MPI_SUM, MPI_COMM_WORLD);
-        MPI_Allreduce(&swaps, &global_swaps, 1, MPI_INT, MPI_LOR, MPI_COMM_WORLD);
-
-        for (size_t c = 0; c < cabinets.count; c++)
-            for (size_t s = 0; s < subject_count; s++)
-                cabinets.scores[c * subject_count + s] = global_count[c] ? global_sum[c * subject_count + s] / (double) global_count[c] : 0.0;
+        MPI_Request request;
+        MPI_Iallreduce(&local_swaps, &global_swaps, 1, MPI_INT, MPI_LOR, col_comm, &request);
+        recompute_scores();
+        MPI_Waitall(1, &request, MPI_STATUS_IGNORE);
 
     } while (global_swaps);
 
@@ -115,32 +151,51 @@ cleanup:
     if (cabinets.doc_count) free(cabinets.doc_count);
     if (cabinets.scores) free(cabinets.scores);
     if (documents.parent_ids) free(documents.parent_ids);
+    if (global_changes) free(global_changes);
+    if (local_changes) free(local_changes);
 
     MPI_Finalize();
     return 0;
 }
 
+void recompute_scores() {
+
+    MPI_Request requests[2];
+    MPI_Iallreduce(local_count, global_count, (int)cabinets.count,
+                   MPI_UNSIGNED_LONG, MPI_SUM, col_comm, &requests[0]);
+    MPI_Iallreduce(local_sum, global_sum, (int)(cabinets.count * subject_count),
+                  MPI_DOUBLE, MPI_SUM, col_comm, &requests[1]);
+    MPI_Waitall(2, requests, MPI_STATUS_IGNORE);
+
+    for (size_t c = 0; c < cabinets.count; c++)
+        for (size_t s = 0; s < subject_count; s++)
+            cabinets.scores[c * subject_count + s] = global_count[c] ? global_sum[c * subject_count + s] / (double) global_count[c] : 0.0;
+}
+
 void gather_parent_ids() {
+    if (col != 0) return;
 
     int *recv_counts = NULL;
     int *displs = NULL;
 
     if (rank == 0) {
-        recv_counts = malloc(nprocs * sizeof(int));
-        displs = malloc(nprocs * sizeof(int));
+        recv_counts = malloc(grid_rows * sizeof(int));
+        displs = malloc(grid_rows * sizeof(int));
 
-        size_t base = documents.count / nprocs;
-        size_t remainder = documents.count % nprocs;
-
-        for (int i = 0; i < nprocs; i++) {
-            recv_counts[i] = (int)(base + (i < remainder ? 1 : 0));
-            displs[i] = (int)(i * base + (i < remainder ? i : remainder));
+        for (int r = 0; r < grid_rows; r++) {
+            size_t rs, re, rc;
+            compute_partition(documents.count, grid_rows, r, &rs, &re, &rc);
+            recv_counts[r] = (int)rc;
+            displs[r] = (int)rs;
         }
     }
 
-    MPI_Gatherv(&documents.parent_ids[doc_s], (int)(doc_e - doc_s), MPI_UNSIGNED_LONG,
-                documents.parent_ids, recv_counts, displs, MPI_UNSIGNED_LONG,
-                0, MPI_COMM_WORLD);
+    MPI_Gatherv(&documents.parent_ids[doc_s], (int)local_doc_count,
+                MPI_UNSIGNED_LONG, documents.parent_ids, recv_counts, displs,
+                MPI_UNSIGNED_LONG, 0, col_comm);
+
+    if (recv_counts) free(recv_counts);
+    if (displs) free(displs);
 }
 
 void assign_to_cabinets() {
@@ -158,47 +213,32 @@ void assign_to_cabinets() {
                 continue;
             }
 
-            cabinets.scores[idx] =
-                (cabinets.scores[idx] * (double) doc_count + documents.scores[i * subject_count + j])
-                    / (double) new_doc_count;
+            cabinets.scores[idx] = (cabinets.scores[idx] * (double)doc_count +
+                                    documents.scores[i * subject_count + j]) /
+                                   (double)new_doc_count;
         }
         cabinets.doc_count[cab_idx] = new_doc_count;
     }
 }
 
-int reassign_documents() {
-
-    int swaps = 0;
-    memset(local_sum, 0, cabinets.count * subject_count * sizeof(double));
-    memset(local_count, 0, cabinets.count * sizeof(size_t));
+void compute_best_local_cabinet_for_documents() {
 
     for (size_t d = doc_s; d < doc_e; d++) {
-        size_t old_cab_idx = documents.parent_ids[d];
-        size_t new_cab_idx = get_closest_cabinet_index(
-            documents.parent_ids[d], &documents.scores[d * subject_count]);
-
-        if (new_cab_idx != old_cab_idx) swaps = 1;
-
-        documents.parent_ids[d] = new_cab_idx;
-
-        local_count[new_cab_idx]++;
-        for (int s = 0; s < subject_count; s++)
-            local_sum[new_cab_idx * subject_count + s] +=
-                documents.scores[d * subject_count + s];
+        double distance = 0;
+        size_t cab_idx = get_closest_local_cabinet_index(documents.parent_ids[d], &documents.scores[d * subject_count], &distance);
+        local_changes[d - doc_s] = (Change){cab_idx, distance};
     }
-
-    return swaps;
 }
 
-size_t get_closest_cabinet_index(size_t parent_id, double *document_scores) {
-    double min_distance = INFINITY;
+size_t get_closest_local_cabinet_index(size_t parent_id, double *document_scores, double* distance) {
+    *distance = INFINITY;
 
-    for (size_t j = 0; j < cabinets.count; j++) {
-        double distance = calculate_distance(
+    for (size_t j = cab_s; j < cab_e; j++) {
+        double calculated_distance = calculate_distance(
             &cabinets.scores[j * subject_count], document_scores);
 
-        if (distance < min_distance) {
-            min_distance = distance;
+        if (calculated_distance < *distance) {
+            *distance = calculated_distance;
             parent_id = j;
         }
     }
@@ -219,8 +259,8 @@ double calculate_distance(double *score_1, double *score_2) {
 
 bool init_cabinets(Problem *problem) {
     cabinets.count = problem->cabinet_count;
-    cabinets.scores =
-        (double *)calloc(problem->cabinet_count * problem->subject_count, sizeof(double));
+    cabinets.scores = (double *)calloc(
+        problem->cabinet_count * problem->subject_count, sizeof(double));
     cabinets.doc_count = (size_t *)calloc(cabinets.count, sizeof(size_t));
 
     return cabinets.scores && cabinets.doc_count;
@@ -293,11 +333,71 @@ void free_problem(Problem *p) {
     if (p->document_scores) free(p->document_scores);
 }
 
-void split_documents_by_rank() {
-    size_t base = documents.count / nprocs;
-    size_t remainder  = documents.count % nprocs;
+void compute_comms() {
+    int dims[2] = {0, 0};
+    MPI_Dims_create(n_procs, 2, dims);
+    grid_rows = dims[0];
+    grid_cols = dims[1];
 
-    size_t count = base + (rank < remainder ? 1 : 0);
-    doc_s = rank * base + (rank < remainder ? rank : remainder);
-    doc_e = doc_s + count;
+    int periods[2] = {0, 0};
+    MPI_Comm cart_comm;
+    MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 0, &cart_comm);
+
+    int coords[2];
+    MPI_Cart_coords(cart_comm, rank, 2, coords);
+    row = coords[0];
+    col = coords[1];
+
+    int remain_row[2] = {0, 1};
+    int remain_col[2] = {1, 0};
+    MPI_Cart_sub(cart_comm, remain_row, &row_comm);
+    MPI_Cart_sub(cart_comm, remain_col, &col_comm);
+}
+
+void compute_partition(size_t total, int num_parts, int part_id, size_t *start, size_t *end, size_t *count) {
+    size_t base      = total / (size_t)num_parts;
+    size_t remainder = total % (size_t)num_parts;
+    size_t s = (size_t)part_id * base +
+               ((size_t)part_id < remainder ? (size_t)part_id : remainder);
+    size_t c = base + ((size_t)part_id < remainder ? 1 : 0);
+    *start = s;
+    *end   = s + c;
+    *count = c;
+}
+
+bool init_changes() {
+    local_changes = (Change *)calloc(local_doc_count, sizeof(Change));
+    global_changes = (Change *)calloc(local_doc_count * grid_cols, sizeof(Change));
+    return local_changes && global_changes;
+}
+
+int assign_best_cabinet() {
+
+    int swaps = 0;
+    memset(local_sum, 0, cabinets.count * subject_count * sizeof(double));
+    memset(local_count, 0, cabinets.count * sizeof(size_t));
+
+    for (size_t d = 0; d < local_doc_count; d++) {
+        size_t old_cab_idx = documents.parent_ids[doc_s + d];
+        size_t new_cab_idx = old_cab_idx;
+        double min_distance = INFINITY;
+
+        for (int i = 0; i < grid_cols; i++) {
+            Change *entry = &global_changes[i * local_doc_count + d];
+            if (entry->distance < min_distance) {
+                min_distance = entry->distance;
+                new_cab_idx  = entry->cab_idx;
+            }
+        }
+
+        if (new_cab_idx != old_cab_idx) swaps = 1;
+        documents.parent_ids[doc_s + d] = new_cab_idx;
+
+        local_count[new_cab_idx]++;
+        for (int s = 0; s < subject_count; s++)
+            local_sum[new_cab_idx * subject_count + s] +=
+                documents.scores[(doc_s + d) * subject_count + s];
+    }
+
+    return swaps;
 }
--- test.sh
+++ test.sh
@@ -1,44 +1,58 @@
 #!/usr/bin/env bash
 set -e
 
-MAKE_DIR="./g3/omp/src"                   # directory where the Makefile is
-MAKE_TARGET=""                            # e.g. "project" or leave empty for default target
-DEFAULT_BINARY="./g3/omp/src/docs-omp"    # binary produced by make
-
+MAKE_DIR="./g3/omp/src"
+MAKE_TARGET=""
+DEFAULT_BINARY="./g3/omp/src/docs-omp"
 
 if [ $# -eq 0 ]; then
     echo "No binary provided. Building with make..."
-
     if [ -n "$MAKE_TARGET" ]; then
         make -C "$MAKE_DIR" "$MAKE_TARGET"
     else
         make -C "$MAKE_DIR"
     fi
-
     binary="$DEFAULT_BINARY"
+    test_args=()
+elif [ "$1" = "mpirun" ] || [ "$1" = "mpiexec" ]; then
+    # Consume everything up to and including the actual binary
+    # e.g. mpirun -n 4 ./docs-mpi
+    cmd=()
+    while [ $# -gt 0 ]; do
+        cmd+=("$1")
+        # Stop after we hit something that looks like the binary (starts with ./ or /)
+        if [[ "$1" == ./* || "$1" == /* ]] && [ "${#cmd[@]}" -gt 1 ]; then
+            shift
+            break
+        fi
+        shift
+    done
+    binary=("${cmd[@]}")
+    test_args=("$@")
 else
-    binary=$1
+    binary=("$1")
     shift
+    test_args=("$@")
 fi
 
 echo "Running tests..."
 
-if [ "$#" -eq 0 ]; then
+if [ "${#test_args[@]}" -eq 0 ]; then
     for dir in tests/T*/; do
         infile=$(echo "$dir"*.in)
         outfile=$(echo "$dir"*.out)
 
         echo "==> $infile"
-        "$binary" "$infile" | diff -u "$outfile" -
+        "${binary[@]}" "$infile" | diff -u "$outfile" -
     done
 else
-    for n in "$@"; do
+    for n in "${test_args[@]}"; do
         dir="tests/T$(printf "%02d" "$n")/"
         infile=$(echo "$dir"*.in)
         outfile=$(echo "$dir"*.out)
 
         echo "==> $infile"
-        "$binary" "$infile" | diff -u "$outfile" -
+        "${binary[@]}" "$infile" | diff -u "$outfile" -
     done
 fi
 
